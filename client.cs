2dbf15ebf01c8f474629de43b804ee7a156263da
Zoey Martin <xskexus@gmail.com>
2020-09-25 15:07:51 +0100

    added comments

--- client.cs
+++ client.cs
@@ -1,23 +1,28 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
-static public void main () {
+static void main()
+{
+    // setting server port and ip to connect to
     Int32 port = 22443;
     String server = "127.0.0.1";
-    TcpClient client = new TcpClient (server, port);
-    string host = "hostname " + Dns.GetHostName ();
-    Byte[] data = System.Text.Encoding.ASCII.GetBytes (host);
-    NetworkStream stream = client.GetStream ();
-    stream.Write (data, 0, data.Length);
-
+    // creating the connection
+    TcpClient client = new TcpClient(server, port);
+    // sending hostname to the server
+    string host = "hostname " + Dns.GetHostName();
+    Byte[] data = System.Text.Encoding.ASCII.GetBytes(host);
+    NetworkStream stream = client.GetStream();
+    stream.Write(data, 0, data.Length);
+    // setting the variable to receive data
     data = new Byte[256];
 
     // String to store the response ASCII representation.
     String responseData = String.Empty;
 
     // Read the first batch of the TcpServer response bytes.
-    while (true) {
-        Int32 bytes = stream.Read (data, 0, data.Length);
-        responseData = System.Text.Encoding.ASCII.GetString (data, 0, bytes);
+    while (true)
+    {
+        Int32 bytes = stream.Read(data, 0, data.Length);
+        responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
     }
 }
--- server.py
+++ server.py
@@ -2,23 +2,34 @@
 from twisted.internet import protocol, reactor, endpoints
 from threading import Thread
 from socket import gethostbyaddr
+# preparing the list of clients
 clients = []
+# setting port to listen for connections on
 port = "22443"
+# setting variable to be used by function
 con_id = None
+
+# function to handle listing connections and their ids
 
 
 def list_connections():
     for c in range(len(clients)):
         print(f"{c}: {clients[c].addr.host} {clients[c].hostname}\n")
 
+# Handling connection and disconnection and handling their respective lists
+
 
 class Echo(protocol.Protocol):
+    # adding clients to the list of clients
     def connectionMade(self):
         clients.append(self)
+    # receiving data from a client
 
     def dataReceived(self, data):
+        # setting the hostname
         if data.decode('utf-8').split()[0] == "hostname":
             self.hostname = data.decode('utf-8').split()[1]
+        # still display command line when no hostname is supplied
         else:
             if con_id in range(len(clients)):
 
@@ -32,6 +43,7 @@ class Echo(protocol.Protocol):
         clients.pop(clients.index(self))
 
 
+# Receiving connections and forwarding them to the Echo function
 class EchoFactory(protocol.Factory):
     def buildProtocol(self, addr):
         print(
@@ -39,42 +51,56 @@ class EchoFactory(protocol.Factory):
         Echo.addr = addr
         return Echo()
 
+# command line
 
-def thr_test():
+
+def thread_test():
     n = ""
     con_id = None
+    # adds the exit command
     while n != "exit":
+        # if there is no client it hides the hostname
         try:
             n = input(f"{clients[con_id].hostname}> ")
         except:
             n = input(f"> ")
+        # if there are no clients don't process commands
         if len(clients) < 1:
             print("no connections")
             continue
+        # if there isn't a command ask for a command
         if n == "":
             print("please enter a command")
             continue
-
+        # if the command is clients list the connections
         if n == "clients":
             list_connections()
+        # select a client
         elif n.split()[0] == "select":
             try:
+                # if the client isn't found reply client not found
                 if int(n.split()[1]) not in range(len(clients)):
                     print("Client not found")
+                # but if it is correct set the connection id to the client
                 else:
                     con_id = int(n.split()[1])
+            # if the supplied number isn't valid then exit
             except:
                 print("please enter a valid number")
+        # if not a command that can be handled by the server send it to the client
         else:
+            # if no client is selected send it to them all
             if con_id not in range(len(clients)):
                 for i in range(len(clients)):
                     clients[i].transport.write(str.encode(n) + b"\n")
+            # send it to the client selected
             else:
                 clients[con_id].transport.write(str.encode(n) + b"\n")
 
 
+# starting listening
 endpoints.serverFromString(reactor, f"tcp:{port}").listen(EchoFactory())
-thr1 = Thread(target=thr_test)
+thr1 = Thread(target=thread_test)
 thr1.start()
 
 reactor.run()
